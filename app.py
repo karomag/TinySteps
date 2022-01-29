4891c7a69e6051316c048bc2aeebb284f07122c7
karomag <roma-novikov@mail.ru>
2022-01-29 11:29:10 +0300

    add routes

--- app.py
+++ app.py
@@ -4,8 +4,43 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return 'Главная'
+
+
+@app.route('/all')
+def all_page():
+    return 'преподаватели'
+
+
+@app.route('/goals/<goal>/')
+def goal_page(goal):
+    return f'цель {goal}'
+
+
+@app.route('/profiles/<teacher_id>/')
+def teacher_page(teacher_id):
+    return f'преподаватель {teacher_id}'
+
+
+@app.route('/request/')
+def request_page():
+    return 'заявка на подбор'
+
+
+@app.route('/request_done/')
+def request_done_page():
+    return 'заявка на подбор отправлена'
+
+
+@app.route('/booking/<teacher_id>/<day>/<time>/')
+def booking_page(teacher_id, day, time):
+    return f'форма бронирования {teacher_id} на {day} {time}'
+
+
+@app.route('/booking_done/')
+def booking_done_page():
+    return 'заявка отправлена'
 
 
 if __name__ == '__main__':
