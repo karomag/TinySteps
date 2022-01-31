f6fec315625b08e8b6e4a020e3bf20e23f304e89
karomag <roma-novikov@mail.ru>
2022-01-31 23:10:28 +0300

    edited routs

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index.html/')
 def index():
     return 'Главная'
 
 
-@app.route('/all')
+@app.route('/all/')
 def all_page():
     return 'преподаватели'
 
